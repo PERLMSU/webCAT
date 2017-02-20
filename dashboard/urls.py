45c24ad61d2054ab34276ab43eb83b4c2dd4578c
hnarowski13@gmail.com <narowsk4@cse.msu.edu>
2017-02-19 22:57:36 -0500

    organizing ish

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,9 @@ from . import views
 
 urlpatterns = [
   	  url(r'^', views.view_dashboard, name='admin-dashboard'),
+  	  url(r'^charts/', views.view_charts, name='admin-charts'),
+  	  url(r'^tables/', views.view_tables, name='admin-tables'),
+  	  url(r'^forms/', views.view_forms, name='admin-forms'),
+  	  url(r'^bootstrap/', views.view_bootstrap, name='admin-bootstrap'),
+  	  url(r'^blank/', views.view_blank, name='admin-blank'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -5,3 +5,21 @@ from django.shortcuts import render
 
 def view_dashboard(request):
 	return render(request, "index.html")
+
+
+def view_charts(request):
+	return render(request, "charts.html")	
+
+def view_tables(request):
+	return render(request, "tables.html")
+
+
+def view_forms(request):
+	return render(request, "forms.html")
+
+def view_bootstrap(request):
+	return render(request, "bootstrap-elements.html")
+
+
+def view_blank(request):
+	return render(request, "blank-page.html")				
