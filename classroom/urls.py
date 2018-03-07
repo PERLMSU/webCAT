c021166ae67a99492e60b7919729ab1a65066711
hnarowski13@gmail.com <narowsk4@cse.msu.edu>
2018-03-07 10:59:32 -0600

    documentation and get rid of unnecessary code

--- classroom/urls.py
+++ classroom/urls.py
@@ -12,18 +12,9 @@ urlpatterns = [
     url(r'^remove_students/(?P<pk>\d+)/$', views.DeleteAllStudentsView.as_view(), name='classroom-nuke-students'),
     url(r'^add_group', views.add_group, name='classroom-create-group'),
     url(r'^upload/student/$', views.UploadStudentsView.as_view(), name='classroom-upload-students'),
-    url(r'^upload/group/$', views.UploadGroupsView.as_view(), name='classroom-upload-groups'),
     url(r'^assign/$', views.AssignGroupsView.as_view(), name='classroom-assign-groups'),
     url(r'^assign/(?P<pk>\d+)/$', views.AssignMultipleGroupsView.as_view(), name='classroom-assign-multiple-groups'),
     url(r'^assign_students/(?P<pk>\d+)/$', views.AssignMultipleStudentsView.as_view(), name='classroom-assign-multiple-students'),
     url(r'^students/delete/(?P<pk>\d+)/$', views.DeleteStudent.as_view(), name="delete-student"),
     url(r'^groups/delete/(?P<pk>\d+)/$', views.DeleteGroup.as_view(), name="delete-group"),
-    url(r'^download/(.*)', views.download, name="download"),
-    url(r'^download_attachment/(.*)/(.*)', views.download_as_attachment,
-        name="download_attachment"),
-    url(r'^exchange/(.*)', views.exchange, name="exchange"),
-    url(r'^parse/(.*)', views.parse, name="parse"),
-    url(r'^import/', views.import_data, name="import"),
-    url(r'^import_sheet/', views.import_sheet, name="import_sheet"),
-    url(r'^export/(.*)', views.export_data, name="export")
 ]
--- classroom/views.py
+++ classroom/views.py
@@ -131,6 +131,7 @@ def edit_classroom(request, pk):
         return HttpResponseRedirect(reverse('dash-home'))
 
 
+# Sets the number of rotation groups in a particular rotation.
 def add_group(request):
 	form = AddGroupForm(request.POST or None)
 	userid = request.user.id
@@ -240,7 +241,7 @@ class UploadFileForm(forms.Form):
     classroom = forms.ModelChoiceField(queryset=Classroom.objects.all(),widget=forms.HiddenInput())
 
 
-
+# Assign / Unassign mulitple rotation groups from an instructor in a particular rotation
 class AssignMultipleGroupsView(LoginRequiredMixin, SuperuserRequiredMixin, TemplateView):
     """ assign groups to LA's
     """
@@ -257,6 +258,7 @@ class AssignMultipleGroupsView(LoginRequiredMixin, SuperuserRequiredMixin, Templ
                 messages.add_message(self.request, messages.ERROR, 'Unable to assign to this instructor %s' % e)  
                 return HttpResponseRedirect('/classroom/')
 
+            # First, unassign all currently assigned groups from instructor
             assigned_groups = RotationGroup.objects.filter(rotation=rotation,instructor=instructor)
             for group in assigned_groups:
                 group.instructor = None
@@ -265,6 +267,7 @@ class AssignMultipleGroupsView(LoginRequiredMixin, SuperuserRequiredMixin, Templ
             if not checked_groups:
                 messages.add_message(self.request, messages.SUCCESS, 'Successfully unassigned all groups from this instructor.')
 
+            # Second, assign all checked groups to instructor
             for group_pk in checked_groups:
                 #
                 try:
@@ -279,7 +282,7 @@ class AssignMultipleGroupsView(LoginRequiredMixin, SuperuserRequiredMixin, Templ
             messages.error(self.request, form.errors)
             return HttpResponseRedirect('/classroom/')                 
 
-
+# Assign / Unassign mulitple students from a group in a particular rotation
 class AssignMultipleStudentsView(LoginRequiredMixin, SuperuserRequiredMixin, TemplateView):
     """ assign groups to LA's
     """
@@ -296,17 +299,20 @@ class AssignMultipleStudentsView(LoginRequiredMixin, SuperuserRequiredMixin, Tem
                 messages.add_message(self.request, messages.ERROR, 'Unable to assign to this group %s' % e)  
                 return HttpResponseRedirect('/classroom/')
 
+            # First, clear all current students from the rotation group 
             group.students.clear()
 
             count = group.students.count()
             successfully_added = []
+
+            # Second, add all checked students to the rotation group
             for student_pk in checked_students:
                 
                     try:
                         student = Student.objects.get(id = student_pk)
                         if count != MAX_STUDENTS:
                             exists =  RotationGroup.objects.filter(rotation=group.rotation,students__id=student.id)
-                            #remove existing students from other groups
+                            # Remove existing students from other groups
                             for exist in exists:
                                 exist.students.remove(student)
 
@@ -330,7 +336,7 @@ class AssignMultipleStudentsView(LoginRequiredMixin, SuperuserRequiredMixin, Tem
             return HttpResponseRedirect('/classroom/')     
 
 
-
+# Assign one particular rotation group to an instructor 
 class AssignGroupsView(LoginRequiredMixin, SuperuserRequiredMixin, TemplateView):
     """ assign groups to LA's
     """
@@ -358,7 +364,7 @@ class AssignGroupsView(LoginRequiredMixin, SuperuserRequiredMixin, TemplateView)
             messages.error(self.request, form.errors)
             return HttpResponseRedirect('/classroom/')              
 
-
+# Excel upload of students - must use the correct excel template to be successful
 class UploadStudentsView(LoginRequiredMixin, SuperuserRequiredMixin, TemplateView):
     """ upload students
     """
@@ -390,6 +396,10 @@ class UploadStudentsView(LoginRequiredMixin, SuperuserRequiredMixin, TemplateVie
         form = UploadFileForm(self.request.POST, self.request.FILES)
         self.count = 0
         self.email_already_exist = 0
+        
+        # Row function to configure each row in excel sheet
+        # Automatically configures classroom and semester based on context
+        # Leave classroom and semester blank in excel sheet (Headers still needed)
         def class_semester_func(row):
 
             row[4] = classroom
@@ -468,65 +478,6 @@ class DeleteGroup(LoginRequiredMixin, SuperuserRequiredMixin, TemplateView):
 
 
 
-class UploadGroupsView(LoginRequiredMixin, SuperuserRequiredMixin, TemplateView):
-    """ upload groups
-    """
-    template_name = 'upload_groups.html'
-    context = {}    
-
-    def get(self, request, *args, **kwargs):
-        form = UploadFileForm()
-
-        classroom = self.request.user.current_classroom
-        if classroom != None:
-
-            students = Student.objects.filter(classroom=classroom).order_by('last_name')
-            groups = Group.objects.filter(classroom=classroom).order_by('group_number')
-            return render(self.request, self.template_name,
-            {
-                'form': form,
-                'add_group_form': AddGroupForm(),
-                'title': 'Excel file upload and download example',
-                'header': ('Please choose any excel file ' +
-                           'from your cloned repository:'),
-                'groups': groups,
-                'classroom': classroom,
-                'students': students,
-                'upload_view': True,
-            }) 
-        else:
-            messages.add_message(self.request, messages.WARNING, 'You are not currently assigned to any classroom. Please contact your administrator to be assigned to a classroom.')            
-            return HttpResponseRedirect(reverse('dash-home'))       
-
-    def post(self, *args, **kwargs):
-        form = UploadFileForm(self.request.POST, self.request.FILES)
-        def classroom_func(row):
-            try:
-                classroom = Classroom.objects.get(id=classroom_pk)
-                row.pop()
-                row.append(classroom)
-            except Classroom.DoesNotExist:
-                classroom = None     
-            #print(row)
-            return row
-        if form.is_valid():
-            classroom_pk = form.cleaned_data['classroom']  
-            self.request.FILES['file'].save_book_to_databasee(
-                model = Group,
-                mapdict= {"Group_Number": "group_number",
-                          "Description": "description",
-                          "Classroom_ID": "classroom"}, 
-                initializer = classroom_func)
-            self.add_message("Groups successfully added!")
-            return HttpResponseRedirect(reverse('classroom-upload-groups'))
-        else:
-            self.add_message("Form not valid, failed to add groups.")          
-            return render(self.request, self.template_name, self.context)
-
-    def add_message(self, text, mtype=25):
-        messages.add_message(self.request, mtype, text) 
-
-
 class ClassroomView(LoginRequiredMixin, SuperuserRequiredMixin, TemplateView):
     """ dashboard page, manage users
     """
@@ -583,158 +534,116 @@ class ClassroomView(LoginRequiredMixin, SuperuserRequiredMixin, TemplateView):
     def add_message(self, text, mtype=25):
         messages.add_message(self.request, mtype, text) 
 
-def upload(request):
-    
-    if request.method == "POST":
 
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            request.FILES['file'].save_to_database(
-                name_columns_by_row=2,
-                model=Student,
-                mapdict=['first_name', 'last_name', 'group_number', 'student_id'])
-            return HttpResponse("OK")
-        else:
-            return HttpResponseBadRequest()  
-        # def group_func(row):
-        #     print row[0]
-        #     try:
-        #         group = Group.objects.filter(group_number=row[0])[0]
-        #     row[0] = q
-        #     return row
-        # if form.is_valid():
-        #     request.FILES['file'].save_book_to_database(
-        #         models=[
-        #             (Student, ['first_name', 'last_name', 'group_number', 'student_id']),
-        #          ]
-        #         )
-
-        # if form.is_valid():
-        #     filehandle = request.FILES['file']
-        #     return excel.make_response(filehandle.get_sheet(), "csv",
-        #                                file_name="download")
-    else:
-        form = UploadFileForm()
-    return render(
-        request,
-        'upload_form.html',
-        {
-            'form': form,
-            'title': 'Excel file upload and download example',
-            'header': ('Please choose any excel file ' +
-                       'from your cloned repository:')
-        })
+# def download(request, file_type):
+#     sheet = excel.pe.Sheet(data)
+#     return excel.make_response(sheet, file_type)
 
 
-def download(request, file_type):
-    sheet = excel.pe.Sheet(data)
-    return excel.make_response(sheet, file_type)
+# def download_as_attachment(request, file_type, file_name):
+#     return excel.make_response_from_array(
+#         data, file_type, file_name=file_name)
 
 
-def download_as_attachment(request, file_type, file_name):
-    return excel.make_response_from_array(
-        data, file_type, file_name=file_name)
+# def export_data(request, atype):
+#     if atype == "sheet":
+#         return excel.make_response_from_a_table(
+#             Question, 'xls', file_name="sheet")
+#     elif atype == "book":
+#         return excel.make_response_from_tables(
+#             [Question, Choice], 'xls', file_name="book")
+#     elif atype == "custom":
+#         question = Question.objects.get(slug='ide')
+#         query_sets = Choice.objects.filter(question=question)
+#         column_names = ['choice_text', 'id', 'votes']
+#         return excel.make_response_from_query_sets(
+#             query_sets,
+#             column_names,
+#             'xls',
+#             file_name="custom"
+#         )
+#     else:
+#         return HttpResponseBadRequest(
+#             "Bad request. please put one of these " +
+#             "in your url suffix: sheet, book or custom")
 
 
-def export_data(request, atype):
-    if atype == "sheet":
-        return excel.make_response_from_a_table(
-            Question, 'xls', file_name="sheet")
-    elif atype == "book":
-        return excel.make_response_from_tables(
-            [Question, Choice], 'xls', file_name="book")
-    elif atype == "custom":
-        question = Question.objects.get(slug='ide')
-        query_sets = Choice.objects.filter(question=question)
-        column_names = ['choice_text', 'id', 'votes']
-        return excel.make_response_from_query_sets(
-            query_sets,
-            column_names,
-            'xls',
-            file_name="custom"
-        )
-    else:
-        return HttpResponseBadRequest(
-            "Bad request. please put one of these " +
-            "in your url suffix: sheet, book or custom")
+# def import_data(request):
+#     if request.method == "POST":
+#         form = UploadFileForm(request.POST,
+#                               request.FILES)
+
+#         def choice_func(row):
+#             q = Question.objects.filter(slug=row[0])[0]
+#             row[0] = q
+#             return row
+#         if form.is_valid():
+#             request.FILES['file'].save_book_to_database(
+#                 models=[Question, Choice],
+#                 initializers=[None, choice_func],
+#                 mapdicts=[
+#                     ['question_text', 'pub_date', 'slug'],
+#                     ['question', 'choice_text', 'votes']]
+#             )
+#             return HttpResponse("OK", status=200)
+#         else:
+#             return HttpResponseBadRequest()
+#     else:
+#         form = UploadFileForm()
+#     return render(
+#         request,
+#         'upload_form.html',
+#         {
+#             'form': form,
+#             'title': 'Import excel data into database example',
+#             'header': 'Please upload sample-data.xls:'
+#         })
 
 
-def import_data(request):
-    if request.method == "POST":
-        form = UploadFileForm(request.POST,
-                              request.FILES)
-
-        def choice_func(row):
-            q = Question.objects.filter(slug=row[0])[0]
-            row[0] = q
-            return row
-        if form.is_valid():
-            request.FILES['file'].save_book_to_database(
-                models=[Question, Choice],
-                initializers=[None, choice_func],
-                mapdicts=[
-                    ['question_text', 'pub_date', 'slug'],
-                    ['question', 'choice_text', 'votes']]
-            )
-            return HttpResponse("OK", status=200)
-        else:
-            return HttpResponseBadRequest()
-    else:
-        form = UploadFileForm()
-    return render(
-        request,
-        'upload_form.html',
-        {
-            'form': form,
-            'title': 'Import excel data into database example',
-            'header': 'Please upload sample-data.xls:'
-        })
+# def import_sheet(request):
+#     if request.method == "POST":
+#         form = UploadFileForm(request.POST,
+#                               request.FILES)
+#         if form.is_valid():
+#             request.FILES['file'].save_to_database(
+#                 name_columns_by_row=2,
+#                 model=Student,
+#                 mapdict=['question_text', 'pub_date', 'slug'])
+#             return HttpResponse("OK")
+#         else:
+#             return HttpResponseBadRequest()
+#     else:
+#         form = UploadFileForm()
+#     return render(
+#         request,
+#         'upload_form.html',
+#         {'form': form})
 
 
-def import_sheet(request):
-    if request.method == "POST":
-        form = UploadFileForm(request.POST,
-                              request.FILES)
-        if form.is_valid():
-            request.FILES['file'].save_to_database(
-                name_columns_by_row=2,
-                model=Student,
-                mapdict=['question_text', 'pub_date', 'slug'])
-            return HttpResponse("OK")
-        else:
-            return HttpResponseBadRequest()
-    else:
-        form = UploadFileForm()
-    return render(
-        request,
-        'upload_form.html',
-        {'form': form})
+# def exchange(request, file_type):
+#     form = UploadFileForm(request.POST, request.FILES)
+#     if form.is_valid():
+#         filehandle = request.FILES['file']
+#         return excel.make_response(filehandle.get_sheet(), file_type)
+#     else:
+#         return HttpResponseBadRequest()
 
 
-def exchange(request, file_type):
-    form = UploadFileForm(request.POST, request.FILES)
-    if form.is_valid():
-        filehandle = request.FILES['file']
-        return excel.make_response(filehandle.get_sheet(), file_type)
-    else:
-        return HttpResponseBadRequest()
-
-
-def parse(request, data_struct_type):
-    form = UploadFileForm(request.POST, request.FILES)
-    if form.is_valid():
-        filehandle = request.FILES['file']
-        if data_struct_type == "array":
-            return JsonResponse({"result": filehandle.get_array()})
-        elif data_struct_type == "dict":
-            return JsonResponse(filehandle.get_dict())
-        elif data_struct_type == "records":
-            return JsonResponse({"result": filehandle.get_records()})
-        elif data_struct_type == "book":
-            return JsonResponse(filehandle.get_book().to_dict())
-        elif data_struct_type == "book_dict":
-            return JsonResponse(filehandle.get_book_dict())
-        else:
-            return HttpResponseBadRequest()
-    else:
-        return HttpResponseBadRequest()
+# def parse(request, data_struct_type):
+#     form = UploadFileForm(request.POST, request.FILES)
+#     if form.is_valid():
+#         filehandle = request.FILES['file']
+#         if data_struct_type == "array":
+#             return JsonResponse({"result": filehandle.get_array()})
+#         elif data_struct_type == "dict":
+#             return JsonResponse(filehandle.get_dict())
+#         elif data_struct_type == "records":
+#             return JsonResponse({"result": filehandle.get_records()})
+#         elif data_struct_type == "book":
+#             return JsonResponse(filehandle.get_book().to_dict())
+#         elif data_struct_type == "book_dict":
+#             return JsonResponse(filehandle.get_book_dict())
+#         else:
+#             return HttpResponseBadRequest()
+#     else:
+#         return HttpResponseBadRequest()
