518c109a3cd8fa097be6dfb42b2a428bb7474c7c
Ben Buscarino <buscari3@msu.edu>
2018-09-11 11:29:40 -0400

    Category test

---
+++ server/test/webcat/feedback/category_test.exs
@@ -0,0 +1,19 @@
+defmodule WebCAT.Feedback.CategoryTest do
+  use WebCAT.DataCase, async: true
+
+  alias WebCAT.Feedback.Category
+  alias WebCAT.Factory
+
+  describe "changeset/2" do
+    test "behaves as expected" do
+      assert Category.changeset(%Category{}, Factory.params_with_assocs(:category)).valid?
+    end
+
+    test "honors unique constraints" do
+      category = Factory.insert(:category)
+
+      {:error, _} =
+        Repo.insert(Category.changeset(%Category{}, Map.from_struct(category)))
+    end
+  end
+end
