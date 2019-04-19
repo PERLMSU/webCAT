13f663811f4a74c9ea28c585b3b554144b824db1
Ben Buscarino <buscari3@msu.edu>
2019-04-19 13:07:23 -0400

    More sensible naming

--- config/prod.exs
+++ config/prod.exs
@@ -32,7 +32,7 @@ config :webcat, WebCAT.Repo,
 config :webcat, WebCAT.Mailer,
   adapter: Bamboo.MailgunAdapter,
   domain: "${MAILGUN_DOMAIN}",
-  api_key: "${MAILGUN_API}"
+  api_key: "${MAILGUN_KEY}"
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -41,41 +41,3 @@ config :cors_plug,
   origin: ["*"],
   max_age: 86_400,
   methods: ["GET", "POST", "PATCH", "DELETE"]
-
-# ## SSL Support
-#
-# To get SSL working, you will need to add the `https` key
-# to the previous section and set your `:url` port to 443:
-#
-#     config :webcat, WebCAT.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [:inet6,
-#               port: 443,
-#               keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#               certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
-#
-# Where those two env variables return an absolute path to
-# the key and cert in disk or a relative path inside priv,
-# for example "priv/ssl/server.key".
-#
-# We also recommend setting `force_ssl`, ensuring no data is
-# ever sent via http, always redirecting to https:
-#
-#     config :webcat, WebCAT.Endpoint,
-#       force_ssl: [hsts: true]
-#
-# Check `Plug.SSL` for all available options in `force_ssl`.
-
-# ## Using releases
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start the server for all endpoints:
-#
-#     config :phoenix, :serve_endpoints, true
-#
-# Alternatively, you can configure exactly which server to
-# start per endpoint:
-#
-#     config :webcat, WebCAT.Endpoint, server: true
-#
